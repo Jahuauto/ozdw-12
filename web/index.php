2cbcc64c06409e0c8e08e314c92afb31373f6a1c
Zygi <jahuauto@gmail.com>
2018-04-03 22:58:13 +0200

    End first day

--- web/index.php
+++ web/index.php
@@ -1,14 +1,25 @@
 <?php
-$controller = isset ($_GET['controller']) ? $_GET['controller'] : 'home';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request  = Request::createFromGlobals();
+$controller = $request->get('controller', 'home');
+$response = new Response();
+
 
 $dir = __DIR__ . '/../src/app/controllers/';
 
-echo $dir . $controller . '.php';
-
 if (file_exists($dir . $controller . '.php')){
+    ob_start();
     require $dir . $controller . '.php';
+    $response->setContent(ob_get_clean());
 }
 else {
-    header("HTTP/1.0 404 Not Found");
-    echo "404";
+   $response->setStatusCode(404);
+   $response->setContent('Nie ma takiej strony');
 }
+
+$response->send();
